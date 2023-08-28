030732fccde8963b32f250538df94035de73a7c3
Jesus Abrahan Pena <jesusapm14@gmail.com>
2023-08-28 13:18:15 -0400

    feat: :art: initial setup

---
+++ harry-potter-app/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
