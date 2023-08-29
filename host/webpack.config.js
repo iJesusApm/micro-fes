1641f062cdedf6b015c247d9addbace7907d77d9
Jesus Abrahan Pena <jesusapm14@gmail.com>
2023-08-29 11:41:28 -0400

    feat: :art: change publicPath

--- host/webpack.config.js
+++ host/webpack.config.js
@@ -4,7 +4,8 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const deps = require('./package.json').dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: 'http://localhost:8080/',
+    publicPath: 'https://micro-fes-host.vercel.app/',
+    // publicPath: 'http://localhost:8080/',
   },
 
   resolve: {
